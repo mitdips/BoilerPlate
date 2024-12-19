72a038676f7d09a3e78a9043da4de4c32269bfc5
mitdips <mitd@itpathsolutions.com>
2024-12-19 10:45:38 +0530

    theme setup, babel config setup, Rn paper setup

---
+++ lib/utils/styled.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-unresolved */
+import { AppTheme } from "@/constants/theme";
+import { ThemedStyledInterface } from "styled-components/index";
+import baseStyled from "styled-components/native";
+
+export const styled = baseStyled as unknown as ThemedStyledInterface<AppTheme>;
