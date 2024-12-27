4813b317ad2048634ab638f6248ca2411ff74696
shyamp109 <shyamp.itpath@gmail.com>
2024-12-27 09:46:18 +0530

    forgot password api integrate

--- app/(public)/ForgotPassword/index.tsx
+++ app/(public)/ForgotPassword/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { LoginFormContainer } from "./ForgotPassword.styles";
 import images from "../../../assets/index";
 import { ForgotPasswordFormData } from "./ForgotPassword.props";
@@ -7,12 +7,31 @@ import FormTemplate from "@templates/FormTemplate/FormTemplate";
 import ForgotPasswordForm from "@organisms/ForgotPasswordForm/ForgotPasswordForm";
 import { ScrollView } from "react-native";
 import { useAppTheme } from "@constants/theme";
-
+import { FireBaseAuth } from "../../../firebase";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { showError, showSuccess } from "@utils/toastMessage";
+import { router } from "expo-router";
 const ForgotPassword = () => {
-  const onForgotPress = async (values: ForgotPasswordFormData) => {
-    console.log("Forgot Press");
-  };
+  const [loading, setLoading] = useState(false);
   const { colors } = useAppTheme();
+  const onForgotPress = async (values: ForgotPasswordFormData) => {
+    const { email } = values;
+    setLoading(true);
+    try {
+      await sendPasswordResetEmail(FireBaseAuth, email);
+      showSuccess("Password reset email sent. Please check your inbox.");
+      router.replace("/(public)/login");
+    } catch (error: any) {
+      if (error.code === "auth/user-not-found") {
+        showError("No user found with this email.");
+      } else if (error.code === "auth/invalid-email") {
+        showError("Invalid email address.");
+      } else {
+        showError("Failed to send password reset email. Please try again.");
+      }
+    }
+    setLoading(false);
+  };
 
   return (
     <ScrollView
@@ -29,6 +48,7 @@ const ForgotPassword = () => {
         <LoginFormContainer>
           <FormTemplate
             Component={ForgotPasswordForm}
+            loading={loading}
             onSubmit={onForgotPress}
           />
         </LoginFormContainer>
